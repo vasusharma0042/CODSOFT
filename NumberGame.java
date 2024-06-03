18b747567cae75cc062c4fe93395879a0e91f918
vasusharma0042 <140676829+vasusharma0042@users.noreply.github.com>
2024-06-03 18:23:24 +0530

    Create NumberGame.java

---
+++ NumberGame.java
@@ -0,0 +1,25 @@
+public class NumberGame{
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int changes = 0;
+       int finals = 0;
+       boolean playAgain = true;
+       System.out.println("you have about"+changes+"to win the game");
+       while(playAgain){
+           int rand = getrandN(1,100);
+           boolean guess = false;
+           for(int i=0;i<changes;i++){
+               boolean guess=false;
+               for(int i=0;i<changes;i++){
+                   System.out.println("changes"+(i+1)+"enter your guess");
+                   int user= sc.nextInt();
+                   if(user==rand){
+                       guess = true;
+                       finals += 1;
+                       System.out.println("you on it.");
+                   }
+               }
+           }
+       }
+    }
+}

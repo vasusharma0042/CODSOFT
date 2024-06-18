da2b850365da5c568a59af3c282c38555aa62f82
vasusharma0042 <140676829+vasusharma0042@users.noreply.github.com>
2024-06-18 18:04:14 +0530

    Update stugent_grade_clacuator.java

--- stugent_grade_clacuator.java
+++ stugent_grade_clacuator.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 class stugent_grade_clacuator{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+
         
         System.out.println("Studrnt Grade claculator");
         System.out.println("Enter number if Subject");

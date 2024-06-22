d85c0632dfdcb68c2dba271fe3db3cb1d2f44595
vasusharma0042 <140676829+vasusharma0042@users.noreply.github.com>
2024-06-22 17:06:46 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,18 +1,16 @@
-{
-    
-    public class NumberGame{
+import java.util.Scanner;
+
+public class NumberGame{
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       int changes = 0;
+       int changes = 8;
        int finals = 0;
        boolean playAgain = true;
        System.out.println("you have about"+changes+"to win the game");
        while(playAgain){
            int rand = getrandN(1,100);
            boolean guess = false;
-           for(int i=0;i<changes;i++){
-               boolean guess=false;
-               for(int i=0;i<changes;i++){
+            for(int i=0;i<changes;i++){
                    System.out.println("changes"+(i+1)+"enter your guess");
                    int user= sc.nextInt();
                    if(user==rand){
@@ -21,8 +19,24 @@
                        System.out.println("you on it.");
                        break;
                    }
-                   
+                   else if(user>rand){
+                       System.out.println("too high");
                }
+                   else{
+                       System.out.println("too low");
+                    }
+                }
+               if(guess == false){
+                   System.out.println("Sorry buddy. please change the number"+rand);
+                }
+               System.out.println("Do you want to play Again");
+               String pA = sc.next();
+               playAgain=pA.equalsIgnoreCase("y");
+           }
+           System.out.println("hope you enjoyed it");
+           System.out.println("here is your Score"+finals);
        }
+        public static int getrandN(int min,int max){
+            return(int)(Math.random()*(max-min+1)+min);
+        }
     }
-}

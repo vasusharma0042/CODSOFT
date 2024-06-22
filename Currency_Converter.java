d23c7eb98e235867a2003d95a428feee18e89063
vasusharma0042 <140676829+vasusharma0042@users.noreply.github.com>
2024-06-22 17:47:24 +0530

    Update Currency_Converter.java

--- Currency_Converter.java
+++ Currency_Converter.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class CurrencyConverter {
 
-  private static final double Exchange_rate = 1.20;
+  private static final double EXCHANGE_RATE = 1.20;
   public static double convertEurotoUSD(double euros) {
     return euros *  EXCHANGE_RATE;
   
@@ -10,7 +10,7 @@ public class CurrencyConverter {
 public static void main(String[]arg){
   Scanner scanner = new Scanner(System.in);
   System.out.println("Enter Amount");
-  double euros = Scanner.nextdouble();
+  double euros = scanner.nextDouble();
 
   double usd = convertEurotoUSD(euros);
   System.out.println(euros + "Euros" + usd + "US Dollor");
